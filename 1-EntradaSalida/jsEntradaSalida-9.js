3029e087541e914f1b5b26b48a6c7a2622758f2f
aldana08 <aldy.pereyra.desa@gmail.com>
2019-02-08 22:14:09 -0300

    ejercicio9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,20 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var sueldos
+    var result
 
-	
+    sueldos=document.getElementById(sueldos).value;
+    result=document.getElementById(result).value;
+
+    sueldos=parseInt(sueldos);
+    result=parseInt(result);
+
+    aumento=parseInt(sueldos) + 10% parseInt(result);
+
+    alert("El resultado con el incremento es " + aumento);
+
+
+    
+    
 }
