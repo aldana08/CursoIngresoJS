1c4122e9b95d40c192dc6faff4df623cc97c8ce6
aldana08 <aldy.pereyra.desa@gmail.com>
2019-02-10 20:22:49 -0300

    ejercicio parcial 1,2,y3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+    var precio
+    var porcentaje
+    var cuenta
+    var preciofinal
+    
+
+    precio = prompt("Ingrese el precio");
+    porcentaje = prompt("Ingrese el porcentaje a calcular");
+
+    precio = parseInt(precio);
+    porcentaje = parseInt(porcentaje);
+
+    cuenta = (precio * porcentaje) / 100;
+
+    preciofinal = precio - cuenta;
+
+    document.getElementById("elPrecioFinal").value = preciofinal;
+
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,21 @@
 
 function mostrar()
 {
+    var dato1
+    var dato2
+    var calcular
+
+    dato1 = prompt("Ingrese el valor de lado uno");
+    dato2 = prompt("ingrese el valor de lado dos");
+
+    dato1 = parseInt(dato1);
+    dato2 = parseInt(dato2);
+
+    calcular = (dato1 + dato2) * 2;
+
+    alert("El perimetro del cuadrado es " + calcular);
+
+
+
 
 }
