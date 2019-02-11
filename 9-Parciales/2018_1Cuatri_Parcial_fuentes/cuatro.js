6b07742b0109856f8fbb007ce61045cdd313622f
aldana08 <aldy.pereyra.desa@gmail.com>
2019-02-10 22:00:48 -0300

    ejercicio 4parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,24 @@
 function mostrar()
 {
+    var num1
+    var num2
 
+    num1 = prompt("Ingrese un primer numero");
+    num2 = prompt("Ingrese un segundo numero");
+
+   if(num1 = num2){
+
+        alert(num1 + " y " + num2 + " Estos numeros son iguales");
+   }
+
+    //num1 = parseInt(num1);
+    //num2 = parseInt(num2);
+
+    if(num1 > num2){
+
+        alert(num1 + " es mayor que " + num2 );
+
+    }
+
+   
 }
