17b98c42da8bbd756bf565978adb3f137688d390
aldana08 <aldy.pereyra.desa@gmail.com>
2019-02-13 01:33:02 -0300

    ejercicio parcial num 5

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,41 @@
 function mostrar()
 {
+    var nombre
+
+    nombre = prompt("Ingrese el nombre de un planeta del sistema solar");
+
+
+    switch (nombre) {
+        case 'mercurio':
+          alert('Aca hace mas calor');
+          break;
+        case 'venus':
+          alert('Aca hace mas calor');
+          break;
+        case 'tierra':
+          alert('Aca vivimos');
+          break;
+        case 'marte':
+          alert('Aca hace mas calor');
+          break;
+          case 'jupiter':
+          alert('Aca hace mas calor');
+          break;
+          case 'saturno':
+          alert('Aca hace mas frio');
+          break;
+          case 'urano':
+          alert('Aca hace mas frio');
+          break;
+          case 'neptuno':
+          alert('Aca hace mas frio');
+          break;
+          
+        default:
+          alert("No es un planeta valido");
+
+    }      
+      
 
 }
+
