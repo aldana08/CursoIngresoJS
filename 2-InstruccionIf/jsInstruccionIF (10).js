c5d7d2ab43c06405d706f69cce45f736364e3adc
aldana08 <aldy.pereyra.desa@gmail.com>
2019-02-15 01:28:50 -0300

    if 8,9, y 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,23 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+	var aleatorio = Math.round(Math.random()*10);
+	alert("Mostrar numero aleatorio entre 1 y 10: " + aleatorio);
+
+	if(aleatorio <=10 && aleatorio >=9 ){
+
+		alert("EXCELENTE");
+	}
+
+	if(aleatorio >=4 && aleatorio <9 ){
+
+		alert("APROBO");
+	}
+
+	if(aleatorio <4 ){
+
+		alert("Vamos, la proxima se puede");
+	}
 
 }//FIN DE LA FUNCIÓN
+
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,6 +2,21 @@ function mostrar()
 {
 //tomo la edad  
 
+var edad
+var estadocivil
+
+
+
+edad = document.getElementById("edad").value;
+estadocivil = document.getElementById("estadoCivil").value;
+
+//alert(estadocivil);
+edad = parseInt(edad);
+
+if(estadocivil == "Soltero" && edad >= 18){
+    alert("Es soltero y no es menor");
+}
+
 	
 
 
