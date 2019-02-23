7ef872f2afbe34f890d842be9115cfa7c60dd58a
aldana08 <aldy.pereyra.desa@gmail.com>
2019-02-22 22:10:59 -0300

    while 9,10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -2,17 +2,48 @@ function mostrar()
 {
 
 	var contador=0;
-	//declarar contadores y variables 
-	
+  //declarar contadores y variables
+  var num;
+  var sumaneg=0;
+  var sumapos=0;
+  var contpos=0;
+  var contneg=0;
+  var contceros=0;
+  var contpares=0;
+
 	var respuesta="si";
 
-	while(respuesta!="no")
+	/*while(respuesta!="no")
 	{
-		
-	
+
+
 	}
 
+*/
 
+do{
+  num=prompt("Ingrese un numero");
+  num=parseInt(num);
+}while(isNaN(num));
+
+if(num <0){
+  sumapos=num;
+  contneg++;
+}else{
+
+  if(num>0){
+    sumapos=num;
+    contpos++;
+  }else{
+    contceros++;
+  }
+} if(num%2 == 0){
+  contpares++;
+}
+  rta=confirm();
+}while(rta);
 
 
 }//FIN DE LA FUNCIÓN
+
+
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,13 +3,47 @@ function mostrar()
 
 	var contador=0;
 	// declarar variables
-	
+  var maximo=0;
+  var minimo=0;
+  var numero=0;
+  var bandera=true;
+
 	var respuesta='si';
 
-	while(respuesta!='no')
+  //while(respuesta!='no')
+
+  while(confirm !==true ){
+
+    do{
+      numero=prompt("Ingrese un numero");
+      numero=parseInt(numero);
+
+    }while(isNaN(numero)){
+
+      if(bandera){
+
+        bandera=false;
+        maximo=numero;
+        minimo=numero;
+      }
+      else{
+
+        if(numero>maximo){
+          maximo=numero;
+        }
+        if(numero<minimo){
+
+          minimo=numero;
+        }
+      }
+
+    }
+  }
+
+
 	{
-		
-	
+
+
 	}
 
 
